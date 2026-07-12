5f58f711885d08052f808af41e989fab00b9d5e5
Isaac Kehle <isaac@kehle.org>
2026-07-11 22:20:59 -0400

    fix: stacks location in init.sjh

--- nextcloud/init.sh
+++ nextcloud/init.sh
@@ -1,2 +1,4 @@
 mkdir -p /volume1/docker/nextcloud/{app,data,db,ts-state,ts-config}
 sudo chown -R $UID:${GROUPS[0]} /volume1/docker/nextcloud
+mkdir -p /volume1/docker/stacks/nextcloud
+sudo chown -R $UID:${GROUPS[0]} /volume1/docker/stacks/nextcloud
--- syncthing/init.sh
+++ syncthing/init.sh
@@ -1,2 +1,4 @@
 mkdir -p /volume1/docker/syncthing/{config,sync,data,ts-state,ts-config}
 sudo chown -R $UID:${GROUPS[0]} /volume1/docker/syncthing
+mkdir -p /volume1/docker/stacks/syncthing
+sudo chown -R $UID:${GROUPS[0]} /volume1/docker/stacks/syncthing
